c4db688a41fe4c6947f209cff6a05a8e3110a65e
guilherme-mertens <mertensdandrade@gmail.com>
2025-01-09 21:40:27 +0100

    Multi-Head Attention

--- model.py
+++ model.py
@@ -56,6 +56,77 @@ class LayerNormalization(nn.Module):
         return self.alpha * (x-mean) / (std + self.eps) + self.bias
 
 
+class FeedForwardBlock(nn.Module):
+    
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.linear2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.linear2(self.dropout(self.relu(self.linear1(x))))
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+
+        assert d_model % h == 0, "d_model is not disiable by h"
+
+        self.d_k = self.d_v = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) --> (Batch, h, seq_len, seq_len)
+        attention_scores = query @ key.transpose(-2, -1) / math.sqrt(d_k)
+
+        if mask is not None:
+            attention_scores.masked_fill_(mask== 0, -1e9)
+        attention_scores = attention_scores.soft_mask(dim = -1)
+        
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return attention_scores @ value, attention_scores
+
+
+    def forward(self, q, k, v, mask=None):
+        query = self.w_q(q) # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+
+        # (Batch, seq_len, d_model) --> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)  # The transpose is for the h to be in the second dimension
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, seq_len, d_k) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1, 2).contigous().view(query.shape[0], -1, self.h*self.d_v)
+
+        return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))  # Check if it's norm applied to the sublayer output or the other way around
 
 if __name__=="__main__":
     a = PositionalEncoding(6, 20, 0.4)
