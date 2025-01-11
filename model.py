c2facf4f96633e9796ce91068cbe80eb450882f5
guilherme-mertens <mertensdandrade@gmail.com>
2025-01-11 22:58:33 +0100

    image in readme

--- model.py
+++ model.py
@@ -107,7 +107,6 @@ class MultiHeadAttentionBlock(nn.Module):
         key = self.w_k(k)
         value = self.w_v(v)
 
-
         # (Batch, seq_len, d_model) --> (Batch, h, seq_len, d_k)
         query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)  # The transpose is for the h to be in the second dimension
         key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
